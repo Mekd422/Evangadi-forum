aaab2d4c8ba249c8b3f7487c13d40c12d96d25a4
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-26 13:07:52 +0300

    routes for the answer question and user pages

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// db connection
+const dbconnection = require('./db/dbconfig');
+// ./routes/userRoute
+const userRouter = require('./routes/user');
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+
+async function testDatabaseConnection() {
+    try {
+        const [rows] = await dbconnection.execute("SELECT 'test' AS message");
+        await app.listen(port);
+        console.log(`Server is running on http://localhost:${port}`);
+         // Log the message from the database
+         console.log("Database connection successful:", rows[0].message);
+       
+        
+    } catch (err) {
+        console.error("Error connecting to the database:", err.message);
+    }
+}
+testDatabaseConnection();
+
+
